79c682fe967de5f3d08048cf5e94a2034a89168a
takesyun0611 <take.shun1616@gmail.com>
2020-09-03 23:33:07 +0900

    トップページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
+  root 'top#index'
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
